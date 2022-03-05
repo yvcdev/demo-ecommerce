b4134d99959cb6c32907c89025b7c9d938dbadfb
yvcdev <95775140+yvcdev@users.noreply.github.com>
2022-03-04 21:55:40 -0500

    Initial setup completed
    Created project structure
    Added getx

---
+++ lib/utils/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  //Colors
+  static const primary = Color.fromRGBO(234, 239, 189, 0);
+  static const secondary = Color.fromRGBO(201, 227, 172, 0);
+  static const primaryAccent = Color.fromRGBO(234, 144, 16, 0);
+  static const secondaryAccent = Color.fromRGBO(144, 190, 109, 0);
+  static const highContrast = Color.fromRGBO(55, 55, 31, 0);
+
+  //Measures
+
+}
