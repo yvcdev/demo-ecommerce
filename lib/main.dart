68154c8795fd458ab1bf955bc8a3dd76c55ca955
yvcdev <95775140+yvcdev@users.noreply.github.com>
2022-03-05 01:00:43 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
         title: 'Demo e-commerce',
-        initialRoute: '/',
         debugShowCheckedModeBanner: false,
         home: Home());
   }
